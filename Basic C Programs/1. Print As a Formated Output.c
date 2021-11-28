24a35eeb3c6f9aab25b58040dda98a3d62d4ef49
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-11-28 09:56:27 +0530

    C Programs

---
+++ Basic C Programs/1. Print As a Formated Output.c
@@ -0,0 +1,20 @@
+// Program To Print As a Formatted Output  //GNU GCC Compiler
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+        printf("\n%-5d %-20s %3d%3d%3d%3d %f",12,"Amitabh",97,93,95,280,97.2123");
+        printf("\n%-5d %-20s %3d%3d%3d%3d %f",14,"Salman",80,85,87,245,81.9645");
+        printf("\n%-5d %-20s %3d%3d%3d%3d %f",15,"Vaishu",98,97,96,345,97.6543");
+        printf("\n%-5d %-20s %3d%3d%3d%3d %f",13,"Baba",87,98,98,234,96.34");
+
+        printf("Thanks!!!");
+
+
+        getch();
+        return 0;
+
+}
