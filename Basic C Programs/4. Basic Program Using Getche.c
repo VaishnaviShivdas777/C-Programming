ec74263411ed99d6e4d6581f4d77c04061376a1f
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-11-30 13:20:29 +0530

    Basic Program

---
+++ Basic C Programs/4. Basic Program Using Getche.c
@@ -0,0 +1,25 @@
+// Basic Program Using getche.c  //GNU GCC Compiler
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch = 'A';
+
+    printf("\n Value Of Character=%c \n\n",ch);
+
+    printf("\n Enter New Character Value:");
+
+    ch = getche();
+
+    printf("\n\n New Value of Chracter=%c \n\n",ch);
+
+    printf("\n Thanks For Using Our App.\n Press a key To Exit...");
+
+    printf("Thank You!!!!");
+
+    getch();
+    return 0;
+
+}
