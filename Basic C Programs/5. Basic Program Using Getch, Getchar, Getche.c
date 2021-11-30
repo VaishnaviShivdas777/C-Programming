6fe2a00e051acca88b54253782113f4f7e16f4ef
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-11-30 13:23:33 +0530

    Basic Program

---
+++ Basic C Programs/5. Basic Program Using Getch, Getchar, Getche.c
@@ -0,0 +1,22 @@
+// Using getch, getchar, getche....//GNU GCC Compiler
+
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+    int No=0;
+    printf("\n\a Value Of Failure=%d",No);
+    getch();
+
+    No=100;
+
+    printf("\n Value Of Success=%d", No);
+
+    printf("\n Thanks For Using Our App. \nPress a key to Exit");
+
+    getch();
+    return 0;
+}
