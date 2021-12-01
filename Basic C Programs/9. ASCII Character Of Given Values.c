e5cbe9b56e70a6c5312123aeddc26e9481a501ea
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-01 14:07:07 +0530

    C Programs

---
+++ Basic C Programs/9. ASCII Character Of Given Values.c
@@ -0,0 +1,23 @@
+// Program To Accept ASCII Character & Display It's Corresponding Value.c   //GNU GCC Compiler....
+
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+ char c;
+
+   printf("\n ASCII Character To Value Conversion=>\n\n");
+
+   printf("\n Enter ASCII Character  to get its corresponding Value = ");
+   scanf("%c",&c);
+
+   printf("\n\n value for Given ASCII Character %c is = |%d|", c,c);
+
+   printf("\n\n Thanks For Using Our Service......");
+
+   getch();
+   return 0;
+}
