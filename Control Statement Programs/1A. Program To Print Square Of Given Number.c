b791e1dc7c7de7f0893af36be9bcfd34785b046c
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-03 10:32:15 +0530

    C Programs

---
+++ Control Statement Programs/1A. Program To Print Square Of Given Number.c
@@ -0,0 +1,23 @@
+// 13.Program To Print Square Of Given Number.c //GNU GCC Compiler
+
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    int No = 0, Sqr = 0;
+
+    printf("\n Enter Number = ");
+    scanf("%d", &No);
+
+    Sqr = No * No;
+
+    printf("\n Square Of %d =%d.", No, Sqr);
+
+    printf("\n\n Thanks");
+
+    getch();
+    return 0;
+}
