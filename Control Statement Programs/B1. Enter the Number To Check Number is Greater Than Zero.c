b2ddd17cd34cfb8f719455b32f79b1705644e926
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-04 13:54:55 +0530

    C Programs

---
+++ Control Statement Programs/B1. Enter the Number To Check Number is Greater Than Zero.c
@@ -0,0 +1,26 @@
+//Enter the Number to check no is greater than zero...//GNU GCC Compiler
+
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    int no;
+
+    printf("Enter the Number= ");
+    scanf("%d",&no);
+
+    if(no>10)
+    {
+
+        printf("No  is greater than zero");
+    }
+    printf("\n no=%d",no);
+
+    printf("\n\nThanks");
+
+    getch();
+
+}
