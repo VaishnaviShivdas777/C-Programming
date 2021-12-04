004e07afdf6d4bd9e6cefd6ea154e61c6c88dc29
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-04 13:57:28 +0530

    C Programs

---
+++ Control Statement Programs/B2. Enter the Number and Identify No is 10.c
@@ -0,0 +1,25 @@
+//Enter the Number and identify No is 10...
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    int no;
+
+    printf("Enter the Number= ");
+    scanf("%d",&no);
+
+    if(no==10)
+    {
+
+        printf("No is 10");
+    }
+    printf("\n no=%d",no);
+
+    printf("\n\nThanks");
+
+    getch();
+
+}
