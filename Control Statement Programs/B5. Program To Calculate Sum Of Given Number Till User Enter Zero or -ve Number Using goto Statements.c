35847ef38506451e3f531e7d2503a00f18e2098c
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-05 12:56:31 +0530

    C Programs

---
+++ Control Statement Programs/B5. Program To Calculate Sum Of Given Number Till User Enter Zero or -ve Number Using goto Statements.c
@@ -0,0 +1,36 @@
+//Program To Calculate Sum Of Given Number Till User Enter Zero or -ve Number Using goto Statements.c  //GNU GCC Compiler....
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    int No = 0, Sum = 0, Cnt = 1;
+
+    printf("\n Enter Number To Calculate Their Sum \n Will Stop Summation After 0/-ve I/P =");
+
+UP:
+        printf("\n\t Enter No %d = ",Cnt);
+        scanf("%d",&No);
+
+        if(No<=0)
+        {
+            goto OUT;
+        }
+
+        Sum = Sum + No;
+        Cnt++;
+
+        goto UP;
+OUT:
+
+    printf("\n Press Any Key To Access get Summation Of Given Number => ");
+    getche();
+
+    printf("\n Summation of Given No=%d",Sum);
+
+    printf("\n\n Thanks!!!");
+    getch();
+    return 0;
+}
