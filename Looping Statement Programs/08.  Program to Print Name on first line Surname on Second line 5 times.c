101860734eee8bff2ce4a275533bc0187a5a7bda
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-05 13:06:17 +0530

    C Programs

---
+++ Looping Statement Programs/08.  Program to Print Name on first line Surname on Second line 5 times.c
@@ -0,0 +1,18 @@
+//program to print NAme on first line surname on second line 5 times.c
+
+#include<conio.h>
+#include<stdio.h>
+
+
+int main()
+{
+    int cnt=0;
+    for(cnt=0;cnt<=5;cnt++)
+    {
+        printf("vaishu \n shivdas \n\n");
+    }
+
+    getche();
+    getch();
+    return 0;
+}
