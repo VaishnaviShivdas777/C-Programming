3433f932f19659fbce9f87095b05d036bd86bafc
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-06 18:05:16 +0530

    C Programs

---
+++ Operator programs/4. Program Using putchar putch.c
@@ -0,0 +1,36 @@
+// Program Using putchar putch.c...// GNU GCC Compiler.....
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    char ch = 'Q', Grade = 'C', Group = 'X';
+
+    putch('\n');
+    putchar('\t');
+
+    printf(" Welcome to Putch/Putchar Demonstration => \n\n");
+
+    putch(ch);
+    putch('\n');
+
+    putchar(ch);
+    putch('\n');
+
+    putch(Group);
+    putch('\n');
+
+    putchar('s');
+    putch('\n');
+
+    putch('KARAD');
+    putch('\n');
+
+    putchar('Grade');
+    putch('\n');
+
+    getch();
+    return 0;
+}
---
+++ Operator programs/5. Program Using Size of.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    char ch='\0';
+    int No=55;
+
+    printf("\n Size For Integer No is=%d",sizeof(No));
+
+    printf("\n Size For character variable ch is=%d",sizeof(ch));
+
+    printf("\n Size For float data type is=%d",sizeof(float));
+
+    printf("\n Size For double data type is=%d",sizeof(double));
+
+    getch();
+    return 0;
+
+}
