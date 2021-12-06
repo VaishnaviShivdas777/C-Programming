9df8741461ef9b622f10306cf049c799f733335d
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-06 17:57:01 +0530

    C Program

---
+++ Operator programs/1. Program To Accept ASCII Value & Display It's Corresponding Character.c
@@ -0,0 +1,22 @@
+//1. Program To Accept ASCII Value & Display It's Corresponding Character...GNU GCC Compiler...
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+   int No = 0;
+
+   printf("\n ASCII To Character Conversion=>\n\n");
+
+   printf("\n Enter ASCII Value to get its corresponding character= ");
+   scanf("%d",&No);
+
+   printf("\n\n character for given ASCII value %d is=|%c|.", No No);
+
+   printf("\n\n Thanks For Using Our Service......");
+
+   getch();
+   return 0;
+}
