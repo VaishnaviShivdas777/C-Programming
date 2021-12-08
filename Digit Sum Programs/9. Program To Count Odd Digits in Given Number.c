49ed49c6e926065da1b011a8cae226974578dfec
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-08 09:34:35 +0530

    C Programs

---
+++ Digit Sum Programs/9. Program To Count Odd Digits in Given Number.c
@@ -0,0 +1,37 @@
+// 9.Program To Count Odd Digits in Given Number.c..//GNU GCC Compiler..
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+            int No = 0, Temp = 0, OCnt = 0, Dig = 0;
+
+            printf("Enter +ve Integer Number To Count Odd Digits in it : ");
+            scanf("%d", &No);
+
+            Temp = No;
+
+            if(No < 0)
+            {
+
+                    printf("Invalid Number"); return -1;
+            }
+
+            while(Temp > 0 )
+            {
+                Dig = Temp %10;
+
+                if(Dig % 2 == 1)
+                {
+                    OCnt++;
+                }
+                Temp = Temp/10;
+            }
+
+            printf("\n Count Of Odd Digits in Given Number %d is = %d.", No, OCnt);
+
+            getch();
+            return 0;
+}
