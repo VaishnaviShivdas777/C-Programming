11cb21a67875fe6cfe40264e4019901a00af0e4a
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-13 19:43:59 +0530

    C Programs

---
+++ 08. Array Programs/03. Array Program.c
@@ -0,0 +1,19 @@
+// Basic Array Program GNU GCC Compiler....
+
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int values[5] = {10, 23, 45, 03, -19};
+
+    printf("\n Value of 1st Element = %d ", values[0]);
+    printf("\n Value of 2nd Element = %d ", values[1]);
+    printf("\n Value of 3rd Element = %d ", values[2]);
+    printf("\n Value of 4th Element = %d ", values[3]);
+    printf("\n Value of 5th Element = %d ", values[4]);
+
+    getch();
+    return 0;
+}
