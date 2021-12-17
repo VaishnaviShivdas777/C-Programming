def97649d83bc256a4f63065ad3d7a3b1353dfcc
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-17 19:41:36 +0530

    C Programs

---
+++ 03. Control Statement Programs/B7. Enter the Number To Check Number is Greater Than Zero.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+int a,b;
+int main()
+{
+
+    printf("\n\nEnter the two values to find the greatest and smallest number: \n");
+    scanf("%d%d", &a, &b);
+
+    if(a == b)
+        printf("Both are equal\n");
+
+    else if(a < b)
+    {
+        printf("\n\nThe largest number is %03d\n", b);
+        printf("\nThe smallest number is %03d\n", a);
+        printf("\nThe largest number is %03d\n", b);
+    }
+    else    //Only possibility remaining
+    {
+        printf("The largest number is %03d\n", a);
+        printf("The smallest number is %03d\n", b);
+    }
+    printf("\n\n\t\t\tThank You..!\n\n\n");
+    return 0;
+}
+
+
+
+
