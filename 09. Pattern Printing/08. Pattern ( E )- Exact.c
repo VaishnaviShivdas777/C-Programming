45e2a2f66b5fa66776f84172f3d0cca1cda2b973
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-27 19:54:50 +0530

    C Programs

---
+++ 09. Pattern Printing/08. Pattern ( E )- Exact.c
@@ -0,0 +1,37 @@
+// Pattern ( E )- Exact.c  Using GNU GCC compiler....
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0, x = 0;
+
+    printf("Enter Row Or Column Values For Pattern: ");
+    scanf("%d", &x);
+
+  printf("\n\n*************Pattern****************************\n");
+
+    for(i = 1; i <= x; i++)
+    {
+        for(j = 1; j <= x; j++)
+        {
+            if(  i == 1 || i == x || j == 1 || ((i == (x/2)+1) && (i >= j)))
+            {
+                printf(" * ");
+            }
+
+            else
+            {
+                printf("   ");
+            }
+
+        }
+         printf("\n");
+    }
+
+    printf("\n\n*************Pattern****************************\n");
+
+    getch();
+    return 0;
+}
