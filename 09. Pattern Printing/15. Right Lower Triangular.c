956816ccf099f0cce07688a7aa5a8a93c9d44c6e
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-28 19:50:38 +0530

    C Programs

---
+++ 09. Pattern Printing/15. Right Lower Triangular.c
@@ -0,0 +1,37 @@
+// Right Lower Triangular.c  Using GNU GCC compiler....
+
+#include<stdio.h>
+#include<conio.h>
+
+    int main()
+    {
+        int i = 0, j = 0, x = 0;
+
+        printf("Enter Row Or Column Values For Pattern: ");
+        scanf("%d", &x);
+
+      printf("\n\n******************Pattern****************************\n");
+
+        for(i = 1; i <= x; i++)
+        {
+            for(j = 1; j <= x; j++)
+            {
+                if( i + j > x )
+                {
+                    printf(" * ");
+                }
+
+                else
+                {
+                    printf("   ");
+                }
+
+            }
+             printf("\n");
+        }
+
+        printf("\n\n*****************Pattern****************************\n");
+
+        getch();
+        return 0;
+    }
