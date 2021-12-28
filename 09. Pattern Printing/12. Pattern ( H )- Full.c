36ff1a22d24268119376c7143d1c4ff4481bdc41
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2021-12-28 19:45:38 +0530

    C Programs

---
+++ 09. Pattern Printing/12. Pattern ( H )- Full.c
@@ -0,0 +1,37 @@
+// Pattern ( H )- Full.c  Using GNU GCC compiler....
+
+#include<stdio.h>
+#include<conio.h>
+
+    int main()
+    {
+        int i = 0, j = 0, x = 0;
+
+        printf("Enter Row Or Column Values For Pattern: ");
+        scanf("%d", &x);
+
+      printf("\n\n******************Pattern****************************\n");
+
+        for(i = 1; i <= x; i++)
+        {
+            for(j = 1; j <= x; j++)
+            {
+                if(  j == 1 || j == x || i == (x/2)+1 )
+                {
+                    printf(" * ");
+                }
+
+                else
+                {
+                    printf("   ");
+                }
+
+            }
+             printf("\n");
+        }
+
+        printf("\n\n*****************Pattern****************************\n");
+
+        getch();
+        return 0;
+    }
