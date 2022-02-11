86a300aed94af2a9894d581aac0107da055b3912
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2022-02-11 23:49:20 +0530

    C Programs

---
+++ 13. Pointer Programs/02. Void Pointer.c
@@ -0,0 +1,14 @@
+///Void Pointer
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    void *vptr;                  /// It is also called as Generic Pointer
+
+    printf("\n Size of Void Pointer : %d", sizeof(vptr));
+
+    getch();
+    return 0;
+}
