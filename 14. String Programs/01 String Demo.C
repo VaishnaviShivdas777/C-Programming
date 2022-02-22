6b7410b8aa01649fad38dd5abc6a916b9bb278b9
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2022-02-22 20:23:39 +0530

    C Programs

---
+++ 14. String Programs/01 String Demo.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+            char City[20] = "Kolhapur City";
+
+            strrev(City);
+
+            printf("\n String = %s.\n", City);
+
+                                                                                     // printf("\n Given String is => %s", cArr);
+
+            _getch();
+            return 0;
+}
