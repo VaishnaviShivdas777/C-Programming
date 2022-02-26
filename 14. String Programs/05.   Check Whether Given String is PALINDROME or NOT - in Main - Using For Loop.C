5e79d722afc1dc999d404e79bff37cb4cc12faed
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2022-02-26 20:19:41 +0530

    String Inside Programs

---
+++ 14. String Programs/05.   Check Whether Given String is PALINDROME or NOT - in Main - Using For Loop.C
@@ -0,0 +1,43 @@
+//Check Whether Given String is PALINDROME or NOT - in Main
+// Using GNU GCC Compiler...
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+            char cSrc[20] ={'\0'};
+            int i = 0, j = 0;
+
+            printf("\n Enter a string : ");
+            gets(cSrc);
+
+            while(cSrc[i] != '\0')
+            {
+                       i++;
+            }
+
+            i--;
+
+            while(i >= j)
+            {
+                    if(cSrc[i] != cSrc[j])
+                    {
+                            break;
+                    }
+                    i--;
+                    j++;
+            }
+
+            if(  i <= j )
+            {
+                    printf("\n Given String is Palindrome");
+            }
+            else
+            {
+                    printf("\n Given String is Not Palindrome");
+            }
+
+            _getch();
+            return 0;
+}
