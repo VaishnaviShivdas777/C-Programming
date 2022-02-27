3f57204f9f0388f66b3c8b03e1b3947a83abcdbe
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2022-02-27 20:11:42 +0530

    String Inside Programs

---
+++ 14. String Programs/09. Count Small Letters in Given String - in Main .C
@@ -0,0 +1,28 @@
+// Count Small Letters  in Given String
+// Using GNU GCC Compiler...
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+            char cSrc[50] ={'\0'};
+            int Small_Cnt = 0, i = 0;
+
+            printf("\n Enter a string : ");
+            gets(cSrc);
+
+            while(cSrc[i] != '\0')
+            {
+                        if( cSrc[i] >= 'a' && cSrc[i] <= 'z')
+                        {
+                                    Small_Cnt++;
+                        }
+                        i++;
+            }
+
+            printf("\n Count Of Small Letters in Given String is = %d.", Small_Cnt);
+
+            getch();
+            return 0;
+}
