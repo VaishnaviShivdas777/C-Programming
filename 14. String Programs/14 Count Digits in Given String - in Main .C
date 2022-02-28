9558b10610f2f0e8adfbc3986853c18e529ab916
Vaishnavi Shivdas <vaishnavi10shivdas@gmail.com>
2022-02-28 22:26:52 +0530

    String Inside Program

---
+++ 14. String Programs/14 Count Digits in Given String - in Main .C
@@ -0,0 +1,29 @@
+// Count of Digits in Given String
+// Using GNU GCC Compiler...
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+            char cSrc[50] ={'\0'};
+            int i = 0, D_Cnt = 0;
+
+            printf("\n Enter a string : ");
+            gets(cSrc);
+
+            while(cSrc[i] != '\0')
+            {
+                        if( cSrc[i] >= '0' && cSrc[i] <= '9' )
+                        {
+                                D_Cnt++;
+                        }
+
+                        i++;
+            }
+
+            printf("\n Count Of Digits in Given String is = %d.", D_Cnt);
+
+            getch();
+            return 0;
+}
